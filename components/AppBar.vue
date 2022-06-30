cd9ed3625a4e6f634c3a677a6eea8f815913361f
Terry Amagboro <rukkiecodes@gmail.com>
2022-06-30 05:56:45 +0100

    done with waitlist

---
+++ components/AppBar.vue
@@ -0,0 +1,55 @@
+<script>
+import { mapActions } from 'vuex'
+export default {
+  methods: {
+    ...mapActions(["toggleDialog"])
+  }
+}
+</script>
+
+<template>
+  <v-app-bar
+    flat
+    color="transparent"
+  >
+    <span class="ml-2 hidden-sm-and-down logo">Oymo</span>
+
+    <v-spacer />
+
+    <v-btn
+      @click="toggleDialog"
+      class="text-capitalize"
+      depressed
+      color="transparent"
+    >
+      About Us
+    </v-btn>
+
+    <v-btn
+      @click="toggleDialog"
+      class="text-capitalize"
+      depressed
+      color="transparent"
+    >
+      Services
+    </v-btn>
+
+    <v-btn
+      @click="toggleDialog"
+      class="text-capitalize"
+      depressed
+      color="transparent"
+    >
+      Parnership
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+
+.logo {
+  font-family: "Pacifico", cursive !important;
+  font-size: 2rem;
+}
+</style>
